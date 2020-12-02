de30a6e9b1f5b96aa55b9527619c9b342195f717
Malachi <malachialusa@gmail.com>
2020-12-02 11:57:20 -0700

    added drag and drop function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,83 @@
 var tasks = {};
 
+//sortable and drag 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    
+  },
+  deactivate: function(event) {
+   
+  },
+  over: function(event) {
+    
+  },
+  out: function(event) {
+    
+  },
+  update: function(event) {
+    //array to store the task data in
+    var tempArr =[];
+
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+    
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+    
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+    
+       // add task data to the temp array as an object
+        tempArr.push({
+        text: text,
+        date: date
+      });
+      
+      console.log(tempArr);
+      
+    });
+
+    // trim down list's ID to match object property
+      var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+      // update array on tasks object and save
+      tasks[arrName] = tempArr;
+      saveTasks();
+
+  }
+  
+  
+});
+
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
+
+
+
+
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
