78697ae8031a1350a986b31731dc1e41706f217c
Malachi <malachialusa@gmail.com>
2020-12-03 12:43:10 -0700

    I added touch ups to button style, and animation to the grabage bin, and font changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,16 +7,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
-    
+    $(this).addClass("dropover")
+    $(".bottom-trash").addClass("bottom-trash-drag")
   },
   deactivate: function(event) {
-   
+   $(this).removeClass("dropover")
+   $(".bottom-trash").removeClass("bottom-trash-drag")
   },
   over: function(event) {
-    
+    $(event.target).addClass("dropover-active")
   },
   out: function(event) {
-    
+    $(event.target).removeClass("dropover-active")
   },
   update: function(event) {
     //array to store the task data in
@@ -81,6 +83,8 @@ var auditTask = function(taskEl) {
  }else if (Math.abs(moment().diff(time, "days")) <= 2) {
   $(taskEl).addClass("list-group-item-warning");
 
+  console.log(taskEl)
+
  }
 
 };
@@ -95,9 +99,10 @@ $("#trash").droppable({
     
   },
   over: function(event, ui) {
-    
+    $(".bottom-trash").addClass("bottom-trash-active")
   },
   out: function(event, ui) {
+    $(".bottom-trash").removeClass("bottom-trash-active")
     
   }
 });
@@ -268,7 +273,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -297,6 +302,12 @@ $("#remove-tasks").on("click", function() {
   }
   saveTasks();
 });
+
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, 1800000);
 
 // load tasks for the first time
 loadTasks();
